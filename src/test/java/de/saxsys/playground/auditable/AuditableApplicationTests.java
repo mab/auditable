26dd4eb05966067fca8a27769e05931c8baf6c69
Matthias Baumgart <matthias.baumgart@saxsys.de>
2020-01-09 16:10:18 +0100

    Initial checkin generated by start.spring.io

---
+++ src/test/java/de/saxsys/playground/auditable/AuditableApplicationTests.java
@@ -0,0 +1,16 @@
+package de.saxsys.playground.auditable;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuditableApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
